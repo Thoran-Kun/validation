be6698ab15380269a340f91dae3c36f62d71d863
Salvatore <pepesalvatore12@gmail.com>
2026-02-12 15:15:48 +0100

    Update ValidationApplication.java

--- src/main/java/com/salvatore/validation/ValidationApplication.java
+++ src/main/java/com/salvatore/validation/ValidationApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ValidationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValidationApplication.class, args);
+		
+        SpringApplication.run(ValidationApplication.class, args);
 	}
 
 }
