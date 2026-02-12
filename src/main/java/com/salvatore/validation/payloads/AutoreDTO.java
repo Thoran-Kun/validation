a7975050ed329ea135ab8d22fbc68e0732bfc495
Salvatore <pepesalvatore12@gmail.com>
2026-02-12 18:01:00 +0100

    Update AutoreDTO.java

--- src/main/java/com/salvatore/validation/payloads/AutoreDTO.java
+++ src/main/java/com/salvatore/validation/payloads/AutoreDTO.java
@@ -9,18 +9,22 @@ public record AutoreDTO(
         @NotBlank(message = "Il nome proprio è un campo obbligatorio")
         @Size(min = 2, max = 30, message = "Il nome proprio deve rientrare tra i 2 e i 30 caratteri")
         String nome,
+
         @NotBlank(message = "Il cognome è un campo obbligatorio")
         @Size(min = 2, max = 30, message = "Il cognome deve essere tra i 2 e i 30 caratteri")
         String cognome,
+
         @NotBlank(message = "La password è obligatoria")
         @Email(message = "la password deve avere almeno 4 caratteri")
         String email,
+
         @NotBlank(message = "La password è obbligatoria")
         @Size(min = 4, message = "La password deve avere almeno 4 caratteri")
+        @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{4,}$",
+                message = "La password deve contenere almeno una maiuscola, una minuscola e un numero")
         String password,
 
         @NotNull(message = "La data di nascita è obbligatoria")
-        @Past(message = "La data di nascita deve essere nel passato")
         @JsonFormat(pattern = "yyyy-MM-dd")
         String dataDiNascita
 ) {
